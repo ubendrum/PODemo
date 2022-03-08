e4800189725ed65dbe00d3200338603fbf5fbf0f
UBENDRUM <UBENDRUM@UBENDRUM-7420.oradev.oraclecorp.com>
2022-03-08 11:22:59 +0530

    Test

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const express = require("express");
+const app = express();
+const hostname = 'localhost';
+const port = 4000;
+const fs = require('fs');
+const bodyParser = require("body-parser");
+const res = require('express/lib/response');
+//app.use(express.static('images')); 
+app.use('/images', express.static('images'));
+//app.use(express.static(path.join(__dirname, 'images')));
+
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  });
+
+  app.get("/",function(req,res){
+    res.sendFile(__dirname + "/home.html");
+ });
+ app.get("/test",function(req,res){
+    res.sendFile(__dirname + "/test.html");
+ });
+ app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(server.address());
+   // ssh1();
+  });
