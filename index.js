02f60c5790c0f520d445a17aaf02faa0f79f6acf
UBENDRUM <UBENDRUM@UBENDRUM-7420.oradev.oraclecorp.com>
2022-03-08 18:45:50 +0530

    first comment

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const port = 4000;
 const fs = require('fs');
 const bodyParser = require("body-parser");
 const res = require('express/lib/response');
-const sql=require('mysql');
+const mysql=require('mysql2');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "ubendrum",
+    password: "fusion",
+    database:'kspd',
+    port: 3306
+  });
+  
+  
 //app.use(express.static('images')); 
 app.use('/images', express.static('images'));
 //app.use(express.static(path.join(__dirname, 'images')));
@@ -18,7 +27,7 @@ app.use('/images', express.static('images'));
     res.end('Hello World');
   });
 
-  app.get("/",function(req,res){
+  app.get("/home",function(req,res){
     res.sendFile(__dirname + "/home.html");
  });
  app.get("/test",function(req,res){
@@ -33,5 +42,26 @@ app.use('/images', express.static('images'));
     res.sendFile(__dirname + "/index.html");
  });
  app.get("/login",function(req,res){
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+      con.query("select * from ksp_users", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
     res.sendFile(__dirname + "/login.html");
+ });
+ app.post("/home",function(req,res){
+    res.sendFile(__dirname + "/home.html");
+ });
+
+ app.post("/addemp",function(req,res){
+    res.sendFile(__dirname + "/addemp.html");
+ });
+ app.post("/updateemp",function(req,res){
+    res.sendFile(__dirname + "/updateemp.html");
+ });
+ app.post("/delemp",function(req,res){
+    res.sendFile(__dirname + "/delemp.html");
  });
